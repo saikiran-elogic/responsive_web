01afb522f92db437fef7ee7f22ebc02b754475c3
Saikiran <sai@sw4.elogic.cad>
2013-12-30 16:03:36 +0530

    Update Code v1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Elogic::Application.config.secret_token = 'bb4e3261b7871db1ef5ba6f32ad4b00b7b2bf467e0471ee4eec93f7529bb3dc35eb28b380832541179b76e4a1c1ff016a9981bb9dbb713601bbac56d7a561839'
